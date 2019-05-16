db5687347b70fdee10cf12f21ea86acd250d7df8
Junger <naydanov_a@mail.ru>
2019-05-16 13:46:36 +0500

    8 lessons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!!!");
+import {config} from './modules/config';
+import AppService from './modules/app.service';
+import './modules/header.component';
+
+import './css/index.css';
+
+console.log("Config key: ", config.key);
+
+const service = new AppService("Hello world!!!");
+service.log();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 4200
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    module: {
+        rules: [
+        {
+            test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+            }
+        }, {
+            test: /\.css$/,
+            use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../',
+                        hmr: process.env.NODE_ENV === 'development',
+                    },
+                },
+                'css-loader',
+            ],
+        }
+        ],
+    },
 };
